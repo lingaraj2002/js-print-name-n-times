62837a758d4e967c661522cc23b0868d341d09de
LINGARAJ <lingaraj01052002@gmail.com>
2023-03-01 12:44:25 +0530

    adding.

---
+++ main.js
@@ -0,0 +1,11 @@
+// Print a name N times
+
+			var name=prompt("Enter the name");
+			var number=Number(prompt("Number of time"));
+
+			function no(){
+				 for(i=1; i<=number; i++){
+					  document.write(name + "<br>");
+					  }
+				 }
+				 no();
